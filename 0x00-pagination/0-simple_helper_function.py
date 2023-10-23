b9c59a3eab2f48594c185163d1ae1c851ff530c0
kelvin-thegreat <oumaklvn2018@gmail.com>
2023-10-23 03:35:57 -0700

    Task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,10 +7,6 @@ def index_range(page, page_size):
     """
     Return a tuple of start and end indexes for a given page and page size.
     Page numbers are 1-indexed.
-
-    :param page: The page number (1-indexed).
-    :param page_size: The number of items per page.
-    :return: A tuple of start and end indexes (0-indexed) for the requested page.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
